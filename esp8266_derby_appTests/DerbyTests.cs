2586c64a8b33f0105c40b2bf63fe98f6785c114e
Jonathan Galentine <j.galentine@gmail.com>
2017-02-15 12:47:09 -0700

    Add nUnit

---
+++ esp8266_derby_appTests/DerbyTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using esp8266_derby_app;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esp8266_derby_app.Tests
+{
+    [TestFixture()]
+    public class DerbyTests
+    {
+        [Test()]
+        public void AddDenTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
